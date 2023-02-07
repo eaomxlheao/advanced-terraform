819812be16fe4b94053fd0cb1007fe4119c19654
EduArdO <eaodevops@outlook.com>
2023-02-07 09:41:49 -0600

    Notes and update

--- main.tf
+++ main.tf
@@ -6,22 +6,23 @@ provider "google" {
 }
 
 ### NETWORK
-data "google_compute_network" "default" {
+resource "google_compute_network" "default" {
   name                    = "default"
+  auto_create_subnetworks = false
 }
 
 ## SUBNET
 resource "google_compute_subnetwork" "subnet-1" {
   name                     = "subnet1"
   ip_cidr_range            = "10.127.0.0/20"
-  network                  = data.google_compute_network.default.id
+  network                  = google_compute_network.default.id
   region                   = "us-central1"
   private_ip_google_access = true
 }
 
 resource "google_compute_firewall" "default" {
   name    = "test-firewall"
-  network = data.google_compute_network.default.id
+  network = google_compute_network.default.id
 
   allow {
     protocol = "icmp"
@@ -49,7 +50,7 @@ resource "google_compute_instance" "nginx_instance" {
   }
 
   network_interface {
-    network = data.google_compute_network.default.self_link
+    network = google_compute_network.default.self_link
     subnetwork = google_compute_subnetwork.subnet-1.self_link
     access_config {
       
@@ -70,7 +71,7 @@ resource "google_compute_instance" "web1" {
 
   network_interface {
     # A default network is created for all GCP projects
-    network = data.google_compute_network.default.self_link
+    network = google_compute_network.default.self_link
     subnetwork = google_compute_subnetwork.subnet-1.self_link
   }
 }
@@ -86,7 +87,7 @@ resource "google_compute_instance" "web2" {
   }
 
   network_interface {
-    network = data.google_compute_network.default.self_link
+    network = google_compute_network.default.self_link
     subnetwork = google_compute_subnetwork.subnet-1.self_link
   }
 }
@@ -102,7 +103,7 @@ resource "google_compute_instance" "web3" {
   }
 
   network_interface {
-    network = data.google_compute_network.default.self_link
+    network = google_compute_network.default.self_link
     subnetwork = google_compute_subnetwork.subnet-1.self_link
   }  
 }
@@ -119,7 +120,7 @@ resource "google_compute_instance" "mysqldb" {
   }
 
   network_interface {
-    network = data.google_compute_network.default.self_link
+    network = google_compute_network.default.self_link
     subnetwork = google_compute_subnetwork.subnet-1.self_link
   }  
 }
